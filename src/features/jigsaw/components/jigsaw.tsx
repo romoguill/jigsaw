f8377ef3b2ba5c25b14a43f8f0729533b1bf5071
romoguill <romoguill@gmail.com>
2025-01-25 15:56:18 -0300

    Created new util for testing shapes with images

--- src/features/jigsaw/components/jigsaw.tsx
+++ src/features/jigsaw/components/jigsaw.tsx
@@ -1,11 +1,11 @@
 import { useMemo } from "react";
+import { generateTestingShapes } from "../../../lib/utils";
 import Canvas from "./canvas";
-import { generateRandomShapes } from "../../../lib/utils";
 
 function Jigsaw() {
   // Generate some shapes for testing
   const shapes = useMemo(() => {
-    return generateRandomShapes(5);
+    return generateTestingShapes();
   }, []);
 
   return <Canvas shapes={shapes} />;
