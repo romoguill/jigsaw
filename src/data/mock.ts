8ef7c67954dcbfc597a8cb405b3d66fa177b5560
romoguill <romoguill@gmail.com>
2025-02-11 09:53:46 -0300

    Added format expected from api and made a mock to use for testing

---
+++ src/data/mock.ts
@@ -0,0 +1,60 @@
+type APIData = { id: string; image: string }[][];
+
+export const mockPuzzleData: APIData = [
+  [
+    {
+      id: "001",
+      image: "/puzzle-images/test-image_1.jpg",
+    },
+    {
+      id: "002",
+      image: "/puzzle-images/test-image_2.jpg",
+    },
+    {
+      id: "003",
+      image: "/puzzle-images/test-image_3.jpg",
+    },
+  ],
+  [
+    {
+      id: "004",
+      image: "/puzzle-images/test-image_4.jpg",
+    },
+    {
+      id: "005",
+      image: "/puzzle-images/test-image_5.jpg",
+    },
+    {
+      id: "006",
+      image: "/puzzle-images/test-image_6.jpg",
+    },
+  ],
+  [
+    {
+      id: "007",
+      image: "/puzzle-images/test-image_7.jpg",
+    },
+    {
+      id: "008",
+      image: "/puzzle-images/test-image_8.jpg",
+    },
+    {
+      id: "009",
+      image: "/puzzle-images/test-image_9.jpg",
+    },
+  ],
+  [
+    {
+      id: "010",
+      image: "/puzzle-images/test-image_10.jpg",
+    },
+    {
+      id: "011",
+      image: "/puzzle-images/test-image_11.jpg",
+    },
+    {
+      id: "012",
+      image: "/puzzle-images/test-image_12.jpg",
+    },
+  ],
+];
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,7 @@ import clsx, { type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { Coordinate } from "../types";
 import { PuzzlePiece } from "../features/jigsaw/puzzle-piece";
+import { mockPuzzleData } from "../data/mock";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -171,3 +172,8 @@ export function generateTestingShapes() {
     shape12,
   ];
 }
+
+// TODO: To be replaced when backend api implemented
+export const getPuzzleData = () => {
+  return mockPuzzleData;
+};
