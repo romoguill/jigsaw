ccae365e385874652400c8fe6dbf94990207bfaf
romoguill <romoguill@gmail.com>
2025-03-21 09:33:23 -0300

    Improved the use image to data hook by maximazing area insted of aspect ratio error. Also added to the return, piece size and the final size of image

--- frontend/src/hooks/use-image-to-game-data.ts
+++ frontend/src/hooks/use-image-to-game-data.ts
@@ -11,31 +11,32 @@ function useImageToGameData({ image, pieceQuantity }: UseImageToGameDataProps) {
   // Get the original size of the image
   const { naturalWidth, naturalHeight } = image.current;
 
-  // Pieces should be squares, so the aspect ratio of the image will determine the ammount of rows and columns based on the piece quantity
-  const aspectRatio = naturalWidth / naturalHeight;
-
   // Equations to solve:
   // 1- Columns x Rows <= PieceQuantity
   // 2- (Rows / Columns) = AspectRatio
   //   const columns = Math.sqrt(pieceQuantity / aspectRatio);
   //   const rows = pieceQuantity / columns;
-  // There is no guarantee to have integer values, must iterate to find biggest pieces possible
-  let bestError;
+  // There is no guarantee to have integer values, must iterate to find biggest pieces possible, or in other words maximize the area of the valid grid vs the image original size
+  let maxArea;
   let finalColumns: number = pieceQuantity;
   let finalRows: number = 1;
+  let finalPieceSize: number = 0;
   for (let columns = 1; columns <= pieceQuantity; columns++) {
     const rows = Math.floor(pieceQuantity / columns);
 
-    if (rows * columns !== pieceQuantity) continue;
+    // Avoid iterating if piece quantity is not satisfied
+    if (pieceQuantity !== rows * columns) continue;
 
-    const newAspectRatio = columns / rows;
+    // Since pieces are squares i need the minimun value from sizes relations
+    const pieceSize = Math.min(naturalWidth / columns, naturalHeight / rows);
+    // Get the area covered by the iteration
+    const gridArea = pieceSize ** 2 * pieceQuantity;
 
-    const error = Math.abs((newAspectRatio - aspectRatio) / aspectRatio);
-
-    if (!bestError || error < bestError) {
-      bestError = error;
+    if (!maxArea || gridArea > maxArea) {
+      maxArea = gridArea;
       finalColumns = columns;
       finalRows = rows;
+      finalPieceSize = pieceSize;
     } else {
       // Function is quadratic meaning that once the error starts increasing is pointless to keep iterating
       break;
@@ -45,7 +46,9 @@ function useImageToGameData({ image, pieceQuantity }: UseImageToGameDataProps) {
   return {
     columns: finalColumns,
     rows: finalRows,
-    pieceSize: naturalWidth / finalColumns,
+    pieceSize: finalPieceSize,
+    adjustedWidth: finalColumns * finalPieceSize,
+    adjustedHeight: finalRows * finalPieceSize,
   };
 }
 
