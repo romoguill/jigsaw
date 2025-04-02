2a68171d569a0ac23dfdc9d997ad650d29ab1df9
romoguill <romoguill@gmail.com>
2025-04-02 11:32:49 -0300

    Added some utils to vector class

--- server/src/core/vector.ts
+++ server/src/core/vector.ts
@@ -1,8 +1,8 @@
 import type { Coordinate } from '@jigsaw/shared/index.js';
 
 export class Vector {
-  readonly x: number;
-  readonly y: number;
+  x: number;
+  y: number;
   constructor(
     public end: Coordinate,
     public start: Coordinate = { x: 0, y: 0 }
@@ -23,5 +23,17 @@ export class Vector {
     const magnitude = this.magnitude;
 
     return new Vector({ x: this.x / magnitude, y: this.y / magnitude });
+  }
+
+  rotateVector90(): Vector {
+    const x = this.x;
+    this.x = -this.y;
+    this.y = x;
+
+    return this;
+  }
+
+  toCoordinate(start: Coordinate): Coordinate {
+    return { x: this.x + start.x, y: this.y + start.y };
   }
 }
