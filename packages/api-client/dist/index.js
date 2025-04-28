178411d28ae746f12ca86d54968708c84fd1d19a
romoguill <romoguill@gmail.com>
2025-04-28 09:57:19 -0300

    Removed some unwanted files

--- packages/api-client/dist/index.js
+++
@@ -1,4 +0,0 @@
-import { hc } from 'hono/client';
-// Recommended by Hono. Build the client to have the types generated.
-const client = hc('');
-export const clientWithType = (...args) => hc(...args);
