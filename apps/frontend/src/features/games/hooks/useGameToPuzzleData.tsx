ea662defa9046d7eac1418295371ae392ddd3fc2
romoguill <romoguill@gmail.com>
2025-04-29 12:39:34 -0300

    Added resuable hook to convert game data to puzzle component interface and integrate it with session/id page

---
+++ apps/frontend/src/features/games/hooks/useGameToPuzzleData.tsx
@@ -0,0 +1,38 @@
+import { GameData, PiecesData } from "@/frontend/types";
+
+import { gameQueryOptions } from "@/frontend/features/jigsaw/api/queries";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
+
+export const useGameToPuzzleData = (gameId: number) => {
+  const { data: gameData } = useSuspenseQuery(
+    gameQueryOptions(gameId.toString())
+  );
+
+  const parsePieceData = useMemo(() => {
+    const piecesData: PiecesData = [];
+
+    gameData.pieces.forEach((piece) => {
+      const rowIndex = piece.row;
+      const colIndex = piece.col;
+
+      if (!piecesData[rowIndex]) {
+        piecesData[rowIndex] = [];
+      }
+
+      piecesData[rowIndex][colIndex] = {
+        id: piece.id.toString(),
+        image: piece.uploadedImage.url,
+      };
+    });
+
+    return piecesData;
+  }, [gameData]);
+
+  const puzzleData: GameData = {
+    pieceSize: gameData.pieceSize,
+    piecesData: parsePieceData,
+  };
+
+  return puzzleData;
+};
--- apps/frontend/src/routes/games/sessions/$sessionId.tsx
+++ apps/frontend/src/routes/games/sessions/$sessionId.tsx
@@ -1,19 +1,31 @@
 import { gameSessionQueryOptions } from "@/frontend/features/games/api/queries";
+import { useGameToPuzzleData } from "@/frontend/features/games/hooks/useGameToPuzzleData";
+import { gameQueryOptions } from "@/frontend/features/jigsaw/api/queries";
+import Puzzle from "@/frontend/features/jigsaw/components/puzzle";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/games/sessions/$sessionId")({
   component: RouteComponent,
   loader: async ({ context, params }) => {
-    context.queryClient.ensureQueryData(
+    const gameState = await context.queryClient.ensureQueryData(
       gameSessionQueryOptions(params.sessionId)
     );
+
+    const gameData = await context.queryClient.ensureQueryData(
+      gameQueryOptions(gameState.gameId.toString())
+    );
+
+    return { gameState, gameData };
   },
 });
 
 function RouteComponent() {
   const { sessionId } = Route.useParams();
-  const { data } = useSuspenseQuery(gameSessionQueryOptions(sessionId));
+  const { data: gameState } = useSuspenseQuery(
+    gameSessionQueryOptions(sessionId)
+  );
+  const piecesData = useGameToPuzzleData(gameState.gameId);
 
-  return <div>Hello "/games/session/$sessionId"!</div>;
+  return <Puzzle puzzleData={piecesData} />;
 }
