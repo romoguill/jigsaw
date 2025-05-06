a5e756dc6de8b0baab9815e4a47db478cd30d3e6
romoguill <romoguill@gmail.com>
2025-05-06 09:07:26 -0300

    Finished 3 step form

--- apps/frontend/src/features/customize/game-customization-form.tsx
+++ apps/frontend/src/features/customize/game-customization-form.tsx
@@ -16,43 +16,34 @@ import {
 } from "@jigsaw/shared";
 import { useState } from "react";
 import UploadInput from "./upload-input";
-import { useForm } from "react-hook-form";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useBuilderCreate } from "../jigsaw-builder/api/mutations";
 
 interface GameCustomizationFormProps {
   step: number;
-  formData: {
-    image: File | null;
-    pieceCount: number | undefined;
-    difficulty: "easy" | "medium" | "hard" | undefined;
-  };
-
   onNext: () => void;
   onBack: () => void;
 }
 
 export function GameCustomizationForm({
   step,
-  formData,
-
   onNext,
   onBack,
 }: GameCustomizationFormProps) {
   const [previewUrl, setPreviewUrl] = useState<string>("");
   const { mutate: buildJigsaw, isPending } = useBuilderCreate();
 
-  const { handleSubmit, control, formState, setValue } =
-    useForm<JigsawBuilderFormValues>({
-      resolver: zodResolver(jigsawBuilderFormSchema),
-      defaultValues: {
-        difficulty: undefined,
-        pieceCount: undefined,
-        borders: true,
-      },
-    });
+  const { handleSubmit, control } = useForm<JigsawBuilderFormValues>({
+    resolver: zodResolver(jigsawBuilderFormSchema),
+    defaultValues: {
+      difficulty: undefined,
+      pieceCount: undefined,
+      borders: true,
+    },
+  });
 
-  const onSubmit = (data: typeof formData) => {
+  const onSubmit: SubmitHandler<JigsawBuilderFormValues> = (data) => {
     // TODO: Handle form submission
     // buildJigsaw({});
     console.log(data);
@@ -61,10 +52,6 @@ export function GameCustomizationForm({
   const handleImageChange = (url: string) => {
     setPreviewUrl(url);
   };
-
-  const handlePieceCountChange = (value: string) => {};
-
-  const handleDifficultyChange = (value: string) => {};
 
   const renderStep = () => {
     switch (step) {
@@ -87,47 +74,66 @@ export function GameCustomizationForm({
       case 2:
         return (
           <div className="space-y-4">
-            <Select
-              selectedKey={formData.pieceCount?.toString()}
-              onSelectionChange={handlePieceCountChange}
-            >
-              <Label>Number of Pieces</Label>
-              <SelectTrigger>
-                <SelectValue className="capitalize" />
-              </SelectTrigger>
-              <SelectPopover>
-                <SelectListBox>
-                  {pieceCount.map((item) => (
-                    <SelectItem key={item} id={item.toString()}>
-                      {item} pieces
-                    </SelectItem>
-                  ))}
-                </SelectListBox>
-              </SelectPopover>
-            </Select>
+            <Controller
+              control={control}
+              name="pieceCount"
+              render={({ field }) => (
+                <Select
+                  {...field}
+                  selectedKey={field.value}
+                  onSelectionChange={(key) => field.onChange(key)}
+                >
+                  <Label>Number of Pieces</Label>
+                  <SelectTrigger>
+                    <SelectValue className="capitalize" />
+                  </SelectTrigger>
+
+                  <SelectPopover>
+                    <SelectListBox>
+                      {pieceCount.map((item) => (
+                        <SelectItem
+                          key={item}
+                          id={item.toString()}
+                          textValue={item}
+                        >
+                          {item}
+                        </SelectItem>
+                      ))}
+                    </SelectListBox>
+                  </SelectPopover>
+                </Select>
+              )}
+            />
           </div>
         );
       case 3:
         return (
           <div className="space-y-4">
-            <Select
-              selectedKey={formData.difficulty}
-              onSelectionChange={handleDifficultyChange}
-            >
-              <Label>Difficulty</Label>
-              <SelectTrigger>
-                <SelectValue className="capitalize" />
-              </SelectTrigger>
-              <SelectPopover>
-                <SelectListBox>
-                  {gameDifficulty.map((item) => (
-                    <SelectItem key={item} id={item}>
-                      {item}
-                    </SelectItem>
-                  ))}
-                </SelectListBox>
-              </SelectPopover>
-            </Select>
+            <Controller
+              control={control}
+              name="difficulty"
+              render={({ field }) => (
+                <Select
+                  {...field}
+                  selectedKey={field.value}
+                  onSelectionChange={(key) => field.onChange(key)}
+                >
+                  <Label>Difficulty</Label>
+                  <SelectTrigger>
+                    <SelectValue className="capitalize" />
+                  </SelectTrigger>
+                  <SelectPopover>
+                    <SelectListBox>
+                      {gameDifficulty.map((item) => (
+                        <SelectItem key={item} id={item}>
+                          {item}
+                        </SelectItem>
+                      ))}
+                    </SelectListBox>
+                  </SelectPopover>
+                </Select>
+              )}
+            />
           </div>
         );
       default:
@@ -137,32 +143,39 @@ export function GameCustomizationForm({
 
   return (
     <div className="w-full">
-      <form onSubmit={handleSubmit(onSubmit)}>{renderStep()}</form>
-      {step > 1 && (
-        <Button variant="outline" onClick={onBack}>
-          Back
-        </Button>
-      )}
-      {step < 3 ? (
-        <Button
-          className="ml-auto flex mt-10"
-          onClick={onNext}
-          disabled={
-            (step === 1 && !formData.image) ||
-            (step === 2 && !formData.pieceCount)
-          }
-        >
-          Next
-        </Button>
-      ) : (
-        <Button
-          className="ml-auto flex"
-          onClick={() => onSubmit(formData)}
-          disabled={!formData.difficulty}
-        >
-          Create Puzzle
-        </Button>
-      )}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {renderStep()}
+        <div className="flex justify-between">
+          {step > 1 && (
+            <Button
+              type="button"
+              variant="secondary"
+              className="flex mt-10"
+              onClick={onBack}
+            >
+              Back
+            </Button>
+          )}
+          {step < 3 ? (
+            <Button
+              type="button"
+              className="ml-auto flex mt-10"
+              onClick={onNext}
+              isDisabled={!previewUrl}
+            >
+              Next
+            </Button>
+          ) : (
+            <Button
+              type="submit"
+              className="ml-auto flex mt-10"
+              onClick={() => {}}
+            >
+              Create Puzzle
+            </Button>
+          )}
+        </div>
+      </form>
     </div>
   );
 }
