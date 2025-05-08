e798a28d7acc5269ae12266698335f367f12d229
romoguill <romoguill@gmail.com>
2025-05-08 18:50:35 -0300

    Changed the db path

--- apps/server/src/db/db.ts
+++ apps/server/src/db/db.ts
@@ -2,11 +2,17 @@ import { drizzle } from 'drizzle-orm/libsql/node';
 import path from 'path';
 import * as schema from './schema.js';
 
-console.log(process.env.DATABASE_URL);
+const isDev = process.env.NODE_ENV === 'development';
+const dbPath = isDev
+  ? path.resolve(process.cwd(), process.env.DATABASE_URL!)
+  : process.env.DATABASE_URL;
+
+console.log('Environment:', process.env.NODE_ENV);
+console.log('Database URL:', dbPath);
 
 export const db = drizzle({
   connection: {
-    url: `file:${path.resolve(process.env.DATABASE_URL!)}`,
+    url: `file:${dbPath}`,
     // authToken:
     //   process.env.NODE_ENV === 'production'
     //     ? process.env.DATABASE_AUTH_TOKEN!
