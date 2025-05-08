b07b069e8c6f5643b438a64e34755ea55e7a4931
romoguill <romoguill@gmail.com>
2025-05-08 17:05:46 -0300

    Removed hardocded db location

--- apps/server/src/db/db.ts
+++ apps/server/src/db/db.ts
@@ -4,11 +4,11 @@ import * as schema from './schema.js';
 
 export const db = drizzle({
   connection: {
-    url: `file:${path.resolve('./data/local.db')}`,
-    authToken:
-      process.env.NODE_ENV === 'production'
-        ? process.env.DATABASE_AUTH_TOKEN!
-        : undefined,
+    url: `file:${path.resolve(process.env.DATABASE_URL!)}`,
+    // authToken:
+    //   process.env.NODE_ENV === 'production'
+    //     ? process.env.DATABASE_AUTH_TOKEN!
+    //     : undefined,
   },
   schema: { ...schema },
 });
