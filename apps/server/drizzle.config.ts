3960a65c8cb26a67e1804956061924c1b367cb90
romoguill <romoguill@gmail.com>
2025-05-08 19:34:08 -0300

    Wrong drizzle config

--- apps/server/drizzle.config.ts
+++ apps/server/drizzle.config.ts
@@ -2,12 +2,22 @@ import dotenv from 'dotenv';
 import path from 'path';
 import { defineConfig } from 'drizzle-kit';
 
-dotenv.config();
+dotenv.config({
+  path: path.resolve(
+    process.cwd(),
+    process.env.NODE_ENV === 'production' ? '.env.production.local' : '.env'
+  ),
+});
+
+const isDev = process.env.NODE_ENV === 'development';
+const dbPath = isDev
+  ? path.resolve(process.cwd(), process.env.DATABASE_URL!)
+  : process.env.DATABASE_URL;
 
 export default defineConfig({
   dialect: 'sqlite',
   schema: './src/db/schema.ts',
   dbCredentials: {
-    url: `file:${path.resolve(process.env.DATABASE_URL!)}`,
+    url: `file:${dbPath}`,
   },
 });
