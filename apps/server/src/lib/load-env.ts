3bdec83fa1f5a7513456ea145d4a4fdc8c48eb09
romoguill <romoguill@gmail.com>
2025-05-08 18:05:12 -0300

    ,,

--- apps/server/src/lib/load-env.ts
+++ apps/server/src/lib/load-env.ts
@@ -1,6 +1,18 @@
 import dotenv from 'dotenv';
 import path from 'path';
 
+console.log(
+  process.cwd(),
+  process.env.NODE_ENV === 'production' ? '.env.production.local' : '.env'
+);
+
+console.log(
+  path.resolve(
+    process.cwd(),
+    process.env.NODE_ENV === 'production' ? '.env.production.local' : '.env'
+  )
+);
+
 dotenv.config({
   path: path.resolve(
     process.cwd(),
