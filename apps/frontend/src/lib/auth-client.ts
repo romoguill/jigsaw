e9b4a41ec8ebf69dac9da71553342dedf9823315
romoguill <romoguill@gmail.com>
2025-05-23 09:10:46 -0300

    Trying with adding frontend env to deploy

--- apps/frontend/src/lib/auth-client.ts
+++ apps/frontend/src/lib/auth-client.ts
@@ -4,8 +4,6 @@ import {
 } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
-console.log(import.meta.env.VITE_SERVER_URL);
-
 export const authClient = createAuthClient({
   baseURL: import.meta.env.VITE_SERVER_URL,
   plugins: [
